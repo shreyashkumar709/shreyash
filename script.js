be54851148d1491d828b7fe900036f3884ed0553
shreyashkumar709 <119500562+shreyashkumar709@users.noreply.github.com>
2023-04-29 12:06:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", (e) => {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+    const links = document.querySelectorAll(".nav-links a");
+    document.body.classList.add('animate');
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("show");
+        links.forEach((link) => {
+            link.addEventListener("click", () => {
+                navLinks.classList.remove("show");
+            });
+        });
+    });
+    const whyText = document.querySelector(".clickable-text p:first-child");
+    const hiddenText = document.querySelector(".clickable-text .hidden-text");
+
+    whyText.addEventListener("click", () => {
+        hiddenText.style.display = "block";
+    });
+    const registerButton = document.getElementById("register-button");
+    registerButton.addEventListener("click", () => {
+        let name = prompt("Please enter your name:");
+        while (name === null || name.trim() === "") {
+            name = prompt("Please enter a valid name:");
+        }
+        alert(`Welcome, ${name}!`);
+    });
+
+})
